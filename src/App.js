46255c0c35c18cbb68f4d0a6001d9011cef585fa
Mankur11 <mankur@inbox.ru>
2019-12-27 14:03:45 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import PatientList from '../src/containers/PatientList';
+
+function App() {
+    return (
+        <div className="App">
+            <PatientList/>
+        </div>
+    );
+}
+
+export default App;
